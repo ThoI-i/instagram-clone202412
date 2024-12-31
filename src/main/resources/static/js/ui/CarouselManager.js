63d33659ef805b982e47032d26739cc148b34034
Theo <ojw1912@gmail.com>
2024-12-31 15:37:37 +0900

    241231 15:36 #09-1-Error [FE/feat] index issue

--- src/main/resources/static/js/ui/CarouselManager.js
+++ src/main/resources/static/js/ui/CarouselManager.js
@@ -8,6 +8,9 @@ class CarouselManager {
 
     // 이미지 트랙(실제 이미지가 배치될 공간)
     this.track = this.container.querySelector('.carousel-track');
+
+    // 인디케이터 영역
+    this.indicatorContainer = this.container.querySelector('.carousel-indicators');
     
     // 실제 이미지 파일 배열
     this.slides = [];
@@ -25,6 +28,7 @@ class CarouselManager {
     // 이미지 트랙 리셋
     this.track.innerHTML = '';
 
+    // 슬라이드 이미지 생성
     this.slides.forEach(file => { 
       // 이미지 생성
       const $img = document.createElement('img');
@@ -37,7 +41,19 @@ class CarouselManager {
       $slideDiv.append($img);
 
       this.track.append($slideDiv);
+
+      // 인디케이터 생성
+      this.makeIndicator(index);
     });
+  }
+
+  // 인디케이터 생성하기
+  makeIndicator() {
+    const $indicator = document.createElement('span');
+    $indicator.classList.add('indicator');
+    if (index === 0) $indicator.classList.add('active');
+
+    this.indicatorContainer.append($indicator);
   }
 
 }

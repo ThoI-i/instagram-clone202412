3553d08f0cca9cd659cc0600d2c358a2bd2e5cbb
Theo <ojw1912@gmail.com>
2024-12-31 12:36:16 +0900

    241231 12:35 #03-2 [FE/feat] Not WORK Scroll During Open Modal

--- src/main/resources/static/js/components/create-feed-modal.js
+++ src/main/resources/static/js/components/create-feed-modal.js
@@ -7,10 +7,8 @@ let elements = {
   $closeBtn: $modal.querySelector('.modal-close-button'),
   $backdrop: $modal.querySelector('.modal-backdrop'),
 };
-
 // 피드 생성 모달 관련 이벤트 함수
 function setUpModalEvents() {
-
   const { $closeBtn, $backdrop } = elements;
 
   // 모달 열기 함수
@@ -18,12 +16,13 @@ function setUpModalEvents() {
     e.preventDefault();
     // 모달 열기
     $modal.style.display = 'flex';
+    document.body.style.overflow = 'hidden';  // 배경 바디 스크롤 방지
   };
-
   // 모달 닫기
-  const closeModal = e => { 
+  const closeModal = e => {
     e.preventDefault();
     $modal.style.display = 'none';
+    document.body.style.overflow = 'auto'; // 배경 바디 스크롤 방지 해제
   };
 
   // 피드 생성 모달 열기 이벤트
@@ -34,16 +33,13 @@ function setUpModalEvents() {
   
   // X버튼 눌렀을 때
   $closeBtn.addEventListener('click', closeModal);
-
   // 백드롭 눌렀을 때
   $backdrop.addEventListener('click', closeModal);
 }
-
 // 이벤트 바인딩 관련 함수
 function bindEvents() {
   setUpModalEvents();
 }
-
 // 모달 관련 JS 함수 - 외부에 노출
 function initCreateFeedModal() {
   bindEvents();

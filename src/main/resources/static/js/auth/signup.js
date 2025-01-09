55f454c0726c7e9778dfc2c04ecaa69dad86ced8
Theo <ojw1912@gmail.com>
2025-01-09 10:25:56 +0900

    250109 10:15 #55-1 [FE/feat] signup empty check

--- src/main/resources/static/js/auth/signup.js
+++ src/main/resources/static/js/auth/signup.js
@@ -20,28 +20,66 @@ function initSignUp() {
   // form submit이벤트
   const $form = document.querySelector('.auth-form');
 
+  // 입력 태그들을 읽어서 객체로 관리
+  const $inputs = {
+    emailOrPhone: $form.querySelector('input[name="email"]'),
+    name: $form.querySelector('input[name="name"]'),
+    username: $form.querySelector('input[name="username"]'),
+    password: $form.querySelector('input[name="password"]'),
+  };
+
+
+  // 4개의 입력창에 입력 이벤트 바인딩
+  Object.values($inputs).forEach($input => { 
+    $input.addEventListener('input', e => { 
+      validateField($input); // 입력값 검증 함수 호출
+    });
+    $input.addEventListener('blur', (e) => {
+      validateField($input); // 입력값 검증 함수 호출
+    });
+  });
+
+
+  // 폼 이벤트 핸들러 바인딩
   $form.addEventListener('submit', e => {
     e.preventDefault(); // 폼 전송시 발생하는 새로고침 방지
 
     // 사용자가 입력한 모든 입력값 읽어오기
-    const emailOrPhone = document.querySelector('input[name="email"]').value;
-    const name = document.querySelector('input[name="name"]').value;
-    const username = document.querySelector('input[name="username"]').value;
-    const password = document.querySelector('input[name="password"]').value;
+    // const emailOrPhone = document.querySelector('input[name="email"]').value;
+    // const name = document.querySelector('input[name="name"]').value;
+    // const username = document.querySelector('input[name="username"]').value;
+    // const password = document.querySelector('input[name="password"]').value;
 
-    const payload = {
-      emailOrPhone: emailOrPhone,
-      name: name,
-      username: username,
-      password: password,
-    };
+    // const payload = {
+    //   emailOrPhone: emailOrPhone,
+    //   name: name,
+    //   username: username,
+    //   password: password,
+    // };
 
     // 서버로 데이터 전송
-    fetchToSignUp(payload);
+    // fetchToSignUp(payload);
     
   });
 
 }
+
+// ==== 함수 정의 ==== //
+// 입력값을 검증하고 에러메시지를 렌더링하는 함수
+function validateField($input) {
+
+  // 1. 빈 값 체크
+  // 이게 어떤태그인지 알아오기
+  const fieldName = $input.name;
+  // 입력값 읽어오기
+  const inputValue = $input.value;
+
+  if (!inputValue) {
+    console.log(fieldName, ' is empty!');
+  }
+
+}
+
 
 //====== 메인 실행 코드 ======//
 document.addEventListener('DOMContentLoaded', initSignUp);

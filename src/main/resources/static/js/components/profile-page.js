df63d21820ce441a4e26b322e4ef939063f00fa9
Theo <ojw1912@gmail.com>
2025-01-15 11:57:45 +0900

    250115 11:56 #85 [FE/feat] profile action button conditional rendering

--- src/main/resources/static/js/components/profile-page.js
+++ src/main/resources/static/js/components/profile-page.js
@@ -2,6 +2,7 @@ import { fetchWithAuth } from '../util/api.js';
 import initCreateFeedModal from './create-feed-modal.js';
 import initMoreMenu from './more-menu.js';
 import initSideBar from './side-bar.js';
+import { getCurrentUser } from '../util/auth.js';
 
 // 이 페이지의 사용자 이름 추출
 function getPageUsername() {
@@ -10,8 +11,15 @@ function getPageUsername() {
   return url.substring(1);
 }
 
+// 현재 페이지에 들어온 사람이 본인인지 확인
+async function isUserMatched() {
+  const pageUsername = getPageUsername();
+  const loggedInUser = await getCurrentUser();
+  return pageUsername === loggedInUser.username;
+}
+
 // 프로필 헤더 렌더링하기
-function renderProfileHeader({ feedCount, name, username, profileImageUrl }) {
+async function renderProfileHeader({ feedCount, name, username, profileImageUrl }) {
   // 프로필 이미지 업데이트
   document.querySelector('.profile-image-container .profile-image img').src =
     profileImageUrl ?? '/images/default-profile.svg';
@@ -26,6 +34,29 @@ function renderProfileHeader({ feedCount, name, username, profileImageUrl }) {
   document.querySelector(
     '.profile-stats li:first-child .stats-number'
   ).textContent = feedCount;
+
+  // 본인의 페이지인지 타인의 페이지인지에 따라 다른 버튼을 렌더링(동적 렌더링)
+  const match = await isUserMatched();
+
+  // 버튼 영역 렌더링
+  const $actionButtonsContainer = document.querySelector('.action-buttons');
+  $actionButtonsContainer.innerHTML = '';
+
+  if (match) {
+    // 본인 프로필인 경우
+    $actionButtonsContainer.innerHTML = `
+                <button class="profile-edit-button">프로필 편집</button>
+                <button class="settings-button">
+                    <i class="fa-solid fa-gear"></i>
+                </button>
+            `;
+  } else {
+    // 타인 프로필인 경우
+    $actionButtonsContainer.innerHTML = `
+                <button class="follow-button">팔로우</button>
+                <button class="message-button">메시지 보내기</button>
+            `;
+  }
 }
 
 // 프로필 페이지 상단부 렌더링 (사용자이름, 프로필사진, 피드 개수, 팔로워 수 등)

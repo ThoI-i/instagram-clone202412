32764e50be935e7ae776299e7a5eccdb2bd342fd
Theo <ojw1912@gmail.com>
2025-01-31 10:24:46 +0900

    250131 10:24 #133 [FE/feat] skeleton loading screen

--- src/main/resources/static/js/components/search-modal.js
+++ src/main/resources/static/js/components/search-modal.js
@@ -1,8 +1,15 @@
+import { debounce } from "../util/debounce.js";
+
+
 const $modal = document.querySelector('.search-modal');
 const $backdrop = document.querySelector('.search-modal-backdrop');
 const $closeBtn = $modal.querySelector('.modal-close');
 const $searchInput = $modal.querySelector('.search-input');
 const $clearBtn = $modal.querySelector('.clear-button');
+
+const $skeletonLoading = $modal.querySelector('.skeleton-loading');
+const $resultList = $modal.querySelector('.search-result-list');
+const $recentSearch = $modal.querySelector('.recent-searches');
 
 // 검색창 모달 닫기
 function closeModal() {
@@ -22,6 +29,34 @@ function openModal() {
   $searchInput.focus();
 }
 
+// 스켈레톤 띄우기
+function showSkeletonLoading() {
+  $recentSearch.style.display = 'none';
+  $skeletonLoading.style.display = 'block';
+}
+
+// 스켈레톤 없애기
+function hideSkeletonLoading() {
+  $skeletonLoading.style.display = 'none';
+  $resultList.style.display = 'block';
+}
+
+
+// 검색 처리 수행
+async function search(inputValue) {
+
+  // 스켈레톤 로딩 스크린 띄우기
+  showSkeletonLoading();
+
+  // 의도적으로 1.5초의 로딩을 걸기
+  await new Promise(resolve => setTimeout(resolve, 1500));
+
+  // 서버 통신
+
+  // 스켈레톤 숨기기
+  hideSkeletonLoading();
+}
+
 // 필요한 이벤트 바인딩
 function bindEvents() {
   // 사이드바의 검색 버튼 클릭시 모달 열기
@@ -38,12 +73,16 @@ function bindEvents() {
   $backdrop.addEventListener('click', closeModal);
 
   // 검색어 입력 이벤트
-  $searchInput.addEventListener('input', e => { 
+  $searchInput.addEventListener('input', debounce(e => { 
     // 입력값 읽기
     const inputValue = e.target.value.trim();
 
     $clearBtn.style.display = inputValue ? 'block' : 'none';
-  });
+
+    if (inputValue) {
+      search(inputValue);
+    }
+  }, 500));
 
   // 검색어 클리어 버튼 이벤트
   $clearBtn.addEventListener('click', () => { 
